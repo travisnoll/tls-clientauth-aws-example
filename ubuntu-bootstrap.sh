3f7556134b357979752d6c81a4628d2ed2a3b5ca
Travis Noll <tjn@umn.edu>
2017-08-02 14:13:06 -0500

    Create ubuntu-bootstrap.sh

---
+++ ubuntu-bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install apache2
+git clone https://github.com/travisnoll/tls-clientauth-aws-example.git ~ubuntu/tls-clientauth-aws-example
+rm /etc/apache2/sites-enabled/*
+cp /home/ubuntu/tls-clientauth-aws-example/tls-clientauth.conf /etc/apache2/sites-available/
+ln -s /etc/apache2/sites-available/tls-clientauth.conf /etc/apache2/sites-enabled/
+sudo a2enmod ssl
+bash ~ubuntu/tls-clientauth-aws-example/server3.sh
+service apache2 restart
