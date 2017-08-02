f99153a2913ae02c145c9eef615b1424e7844c2f
Travis J. Noll <tjn@umn.edu>
2017-08-02 21:29:14 +0000

    Bolster client code and comments.

--- client.sh
+++ client.sh
@@ -3,5 +3,22 @@
 # download certificate authority from server
 openssl s_client -connect $1:443 > ca.cert
 
+# request HEAD.
+curl --head $1
+# This works because we happily respond over http(80).
+
 # request HEAD over ssl.
 curl --head https://$1
+# This fails because our certificate chain doesn't recognize the certificate authority.
+
+# request HEAD over ssl ignoring certificate troubles.
+curl --insecure --head https://$1
+# This fails because we are REQUIRING client-auth on the server.
+
+# request HEAD over ssl using self signed certificate authority.
+curl --head --cacert ./ca.cert https://$1
+# This also fails because we are REQUIRING client-auth on the server.
+
+# request HEAD over ssl using self signed certificate authority.
+curl --head --cacert ./ca.cert --cert ./client-CERT.pem --key ./client-KEY.pem https://$1
+# Finally, this succeeds with a status of 200 OK becuase the client and server trust eachother.
