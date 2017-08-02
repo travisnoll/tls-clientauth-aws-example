19117ebdc6f47fd3751221a0ca14fde5b5e7bb6d
Travis J. Noll <tjn@umn.edu>
2017-08-02 21:20:18 +0000

    Cleanup.  There can be only one server.sh.

--- server.sh
+++ server.sh
@@ -1,30 +1,35 @@
-#!/bin/bash
-
-#get the public hostname from the ec2 instance meta-data
 export PUBLIC_HOSTNAME=`curl -s http://169.254.169.254/latest/meta-data/public-hostname` 
-
-#create the subject for the certificate authority.
 export CERT_SUBJECT="/C=US/ST=Minnesota/L=Minneapolis/O=University of Minnesota/OU=ahcis/CN=$PUBLIC_HOSTNAME"
 
-rm -rf ~/server
-mkdir ~/server
+date | md5sum | awk '{ print $1 }' > phrase.txt
 
-date | md5sum | awk '{ print $1 }' > ~/server/phrase.txt
+#openssl req -x509 -newkey rsa:4096 -passout file:phrase.txt -subj "$CERT_SUBJECT" -out tlsca-CA.pem -keyout tlsca-KEY.pem 
+openssl req -x509 -newkey rsa:4096 -subj "$CERT_SUBJECT" -out tlsca-CA.pem -keyout tlsca-KEY.pem -nodes
 
-export CA_PASSPHRASE=`cat ~/server/phrase.txt`
 
-# generate an RSA private key 
-# www.openssl.org/docs/manmaster/man1/genrsa.html
-#openssl genrsa -des3 -out ~/server/ca.key -passout pass:$CA_PASSPHRASE 4096
-openssl genrsa -des3 -out ~/server/ca.key -passout file:server/phrase.txt 4096
+sudo cp tlsca-CA.pem /etc/ssl/certs/tlsca-ca.crt
+sudo cp tlsca-KEY.pem /etc/ssl/private/tlsca-ca.key
 
-echo $CERT_SUBJECT
 
-# PKCS#10 certificate request and certificate generating utility 
-# www.openssl.org/docs/manmaster/man1/req.html
-openssl req -new -x509 -days 365 -key ~/server/ca.key -out ~/server/ca.crt -passin file:server/phrase.txt -subj "$CERT_SUBJECT"
 
-# publish new certificate and key to ssl.
-sudo cp ~/server/ca.key /etc/ssl/private/
-sudo cp ~/server/ca.crt /etc/ssl/certs/
+export CLIENT_SUBJECT="/C=US/ST=Minnesota/L=Minneapolis/O=University of Minnesota/OU=ahcis/CN=Travis"
+openssl req -newkey rsa:4096 -subj "$CLIENT_SUBJECT" -out client-CA.csr -keyout client-KEY.pem -nodes
+openssl x509 -req -days 730 -in client-CA.csr -CA tlsca-CA.pem -CAkey tlsca-KEY.pem -set_serial 01 -out client-CERT.pem
+
+
+
+#openssl req -x509 -newkey rsa:4096 -subj "$CLIENT_SUBJECT" -out client-CA.pem -keyout client-KEY.pem -nodes
+#openssl x509 -CA tlsca-CA.pem -CAkey tlsca-KEY.pem -in client-CA.pem -out client-signed.pem -set_serial 01
+
+
+
+
+#openssl req -x509 -newkey rsa:4096 -passout file:phrase.txt -subj "$CLIENT_SUBJECT" -out client-CA.pem -keyout client-KEY.pem
+#openssl x509 -CA tlsca-CA.pem -CAkey tlsca-KEY.pem -in client-CA.pem -out client-signed.pem -passin file:phrase.txt -set_serial 01
+#openssl x509 -req -days 365 -in client.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out client.crt
+
+
+#openssl genrsa -out ia.key 4096
+#openssl req -new -key ia.key -out ia.csr
+#penssl x509 -req -days 730 -in ia.csr -CA ca.crt -CAkey ca.key -set_serial 01 -out ia.crt
 
