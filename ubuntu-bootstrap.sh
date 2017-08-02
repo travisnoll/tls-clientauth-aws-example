0b1edb0c6a4569e048cf4329489f2d82eba0b997
Travis Noll <tjn@umn.edu>
2017-08-02 16:09:13 -0500

    Run commands as ubuntu from home directory.

--- ubuntu-bootstrap.sh
+++ ubuntu-bootstrap.sh
@@ -3,13 +3,13 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y install apache2
 
-sudo -H -u ubuntu bash -c 'git clone https://github.com/travisnoll/tls-clientauth-aws-example.git ~ubuntu/tls-clientauth-aws-example' 
+sudo -H -u ubuntu bash -c 'cd; git clone https://github.com/travisnoll/tls-clientauth-aws-example.git ~ubuntu/tls-clientauth-aws-example' 
 
 rm /etc/apache2/sites-enabled/*
 cp /home/ubuntu/tls-clientauth-aws-example/tls-clientauth.conf /etc/apache2/sites-available/
 ln -s /etc/apache2/sites-available/tls-clientauth.conf /etc/apache2/sites-enabled/
 sudo a2enmod ssl
 
-sudo -H -u ubuntu bash -c 'bash ~ubuntu/tls-clientauth-aws-example/server3.sh' 
+sudo -H -u ubuntu bash -c 'cd; bash ~ubuntu/tls-clientauth-aws-example/server3.sh' 
 
 service apache2 restart
